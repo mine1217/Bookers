bda5bf01bb4d32ae15c5dd6fcec5a90b32c36584
Minoru Makino <makino111217@gmail.com>
2021-03-18 11:10:39 +0000

    [ADD]投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,23 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
-  def new
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book)
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.permit(:title, :body)
   end
 end
